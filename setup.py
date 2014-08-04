b02554c0a482dd68985b51b11866d094abe2bbb3
Luar Roji <luar@scrapinghub.com>
2014-08-03 20:28:24 -0700

    Updated command line interface

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 
 setup(name='filehasher',
-      version='0.1',
+      version='0.2',
       description='File Hasher',
       url='http://github.com/cyberplant/filehasher',
       author='Luar Roji',
       author_email='cyberplant@roji.net',
       license='MIT',
-      scripts=['bin/filehasher'],
+      entry_points={
+          'console_scripts': [
+              'filehasher = filehasher.cmdline:main',
+          ]},
       packages=['filehasher'],
-      zip_safe=False)
+      keywords=[
+          'filehasher', 'file', 'hasher', 'dupe'
+      ],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+      zip_safe=True)
