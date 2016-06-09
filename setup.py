e7e1aaa727f1e541d4ac5906b291ba8b2f0ad705
Luar Roji <luar@scrapinghub.com>
2016-06-09 16:45:48 -0300

    Version bump to 0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='filehasher',
-      version='0.3',
+      version='0.4',
       description='File Hasher',
       url='http://github.com/cyberplant/filehasher',
       author='Luar Roji',
